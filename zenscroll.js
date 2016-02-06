35b385a3537df13aa0287b283133b1f74b611b0c
Remi Remino <remi@remino.net>
2016-02-06 14:14:06 +0900

    Round pixels measurement to next integer

--- zenscroll.js
+++ zenscroll.js
@@ -62,7 +62,7 @@
 			if (scrollContainer) {
 				return elem.offsetTop - scrollContainer.offsetTop
 			} else {
-				return elem.getBoundingClientRect().top + getScrollTop() - docElem.offsetTop
+				return Math.ceil(elem.getBoundingClientRect().top + getScrollTop() - docElem.offsetTop)
 			}
 		}
 
